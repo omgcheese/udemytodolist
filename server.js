ab2b3f71a8598969187b351a674f626b9979b60c
Jinwook Lee <jinwook.lee365@gmail.com>
2015-11-16 22:41:17 -0800

    Added todo list and get functionality

--- server.js
+++ server.js
@@ -3,9 +3,54 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id: 1,
+	description: 'Meet mom for lunch',
+	completed: false
+}, {
+	id: 2,
+	description: 'Go to market',
+	completed: false
+}, {
+	id: 3,
+	description: 'buy FO4',
+	completed: true
+}];
+
 app.get('/', function (req, res){
 	res.send('Todo API Root');
 });
+
+// GET /todos
+app.get('/todos', function (req, res) {
+	res.json(todos);
+
+
+});
+
+// GET /todos/:id
+// will return associated todo list
+app.get('/todos/:id', function (req, res){
+	var todoID = parseInt(req.params.id, 10);
+	var matchedTodo;
+	//iterate of todos array. Find the match
+
+	todos.forEach(function (todo) {
+		if (todoID === todo.id) {
+			matchedTodo = todo;
+			res.json(matchedTodo);
+		}
+	});	
+
+	if (!matchedTodo) {
+		res.status(404).send();
+	}
+	//res.send('Asking for todo with id of ' + req.params.id)
+});
+
+
+
+
 
 app.listen(PORT, function() {
 	console.log('Express listening on port ' + PORT);
